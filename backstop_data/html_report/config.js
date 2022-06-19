b1a6d3a84e67012b94db8b8576ac4ba232216c39
Benedicte Quimbert <quimbenedicte@gmail.com>
2022-06-19 18:21:33 +0200

    test: Adds backstopjs for visual regression testing

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,37 +3,147 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "test": "..\\bitmaps_test\\20220612-203622\\backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Homepage_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Homepage_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_phone.png",
-        "label": "BackstopJS Homepage",
+        "fileName": "backstop_default_Kekes_Homepage_0_document_0_phone.png",
+        "label": "Keke's Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "url": "https://garris.github.io/BackstopJS/",
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "phone",
-        "error": "Reference file not found C:\\Users\\quimb\\Desktop\\Dev\\keke-songs\\backstop_data\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_0_phone.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "test": "..\\bitmaps_test\\20220612-203622\\backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Homepage_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Homepage_0_document_1_tablet.png",
         "selector": "document",
-        "fileName": "backstop_default_BackstopJS_Homepage_0_document_1_tablet.png",
-        "label": "BackstopJS Homepage",
+        "fileName": "backstop_default_Kekes_Homepage_0_document_1_tablet.png",
+        "label": "Keke's Homepage",
         "requireSameDimensions": true,
-        "misMatchThreshold": 0.1,
-        "url": "https://garris.github.io/BackstopJS/",
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/",
         "referenceUrl": "",
         "expect": 0,
         "viewportLabel": "tablet",
-        "error": "Reference file not found C:\\Users\\quimb\\Desktop\\Dev\\keke-songs\\backstop_data\\bitmaps_reference\\backstop_default_BackstopJS_Homepage_0_document_1_tablet.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Contact_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Contact_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Kekes_Contact_0_document_0_phone.png",
+        "label": "Keke's Contact",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/contact",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Contact_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Contact_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Kekes_Contact_0_document_1_tablet.png",
+        "label": "Keke's Contact",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/contact",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Terms_of_service_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Terms_of_service_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Kekes_Terms_of_service_0_document_0_phone.png",
+        "label": "Keke's Terms of service",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/terms-of-service",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Kekes_Terms_of_service_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20220619-181924\\backstop_default_Kekes_Terms_of_service_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Kekes_Terms_of_service_0_document_1_tablet.png",
+        "label": "Keke's Terms of service",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 70,
+        "url": "http://localhost:3000/terms-of-service",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
     }
   ],
   "id": "backstop_default"
